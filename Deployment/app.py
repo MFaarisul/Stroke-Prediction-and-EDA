79210983c9874ed197b3e1bf6731e72073993cea
Muhammad Faarisul Ilmi <faarisilmi@gmail.com>
2022-08-19 10:35:01 +0700

    Update file

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import pickle
 import streamlit as st
 from sklearn.preprocessing import StandardScaler
@@ -7,19 +8,11 @@ model = pickle.load(open('Deployment/model.pkl','rb'))
 scaler = pickle.load(open('Deployment/scaler.pkl', 'rb'))
 
 def predict(gender, age, hypertension, heart_disease, ever_married, work_type, residence_type, avg_glucose_level, smoking):
-    # Gender
     gender = 1 if gender == 'Male' else 0
-
-    # Married
     hypertension = 1 if hypertension == 'Yes' else 0
-
-    # Married
     heart_disease = 1 if heart_disease == 'Yes' else 0
-
-    # Married
     ever_married = 1 if ever_married == 'Yes' else 0
 
-    # Work
     if work_type == 'Government':
         work_type = 0
     elif work_type == 'Never worked':
@@ -30,11 +23,8 @@ def predict(gender, age, hypertension, heart_disease, ever_married, work_type, r
         work_type = 3
     else:
         work_type = 4
-    
-    # Residence
-    residence_type = 1 if residence_type == 'Urban' else 0
 
-    # Smoke
+    residence_type = 1 if residence_type == 'Urban' else 0
     smoking = 1 if smoking == 'Smoke' else 0
 
     features = np.array([[gender, age, hypertension, heart_disease, ever_married, work_type, residence_type, avg_glucose_level, smoking]])
@@ -51,30 +41,41 @@ def main():
     '''
     st.markdown(html_temp, unsafe_allow_html=True)
 
-    html_temp2 = '''
-    <br>
-    <p style="font-family: Calibri; font-size: 20px; color: #faedcd;">
-    Fill all the features below and click the predict button to see the result
-    </p>
-    '''
-    st.markdown(html_temp2, unsafe_allow_html=True)
+    st.write('Fill all the features below and click the predict button to see the result')
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
         gender = st.selectbox('Gender', ['Male', 'Female'])
-        age = st.text_input('Age', '')
-        avg_glucose_level = st.text_input('Avg Glucose Level', '')
+        age = st.number_input('Age', step=1)
+        avg_glucose_level = st.number_input('Avg Glucose Level', step=10)
     with col2:
-        heart_disease = st.selectbox('Heart Disease', ['Yes', 'No'])
-        ever_married = st.selectbox('Ever Married', ['Yes', 'No'])
-        hypertension = st.selectbox('Hypertension', ['Yes', 'No'])
-    with col3:
         residence_type = st.selectbox('Residence Type', ['Urban', 'Rural'])
         work_type = st.selectbox('Work Type', ['Private', 'Self employed', 'Children', 'Government', 'Never Worked'])
         smoking = st.selectbox('Smoking Status', ['Smoke', 'Never smoke'])
-
+    with col3:
+        heart_disease = st.selectbox('Heart Disease', ['Yes', 'No'])
+        ever_married = st.selectbox('Ever Married', ['Yes', 'No'])
+        hypertension = st.selectbox('Hypertension', ['Yes', 'No'])
+    
     if st.button("Predict"):
+        st.markdown(
+            """
+            <style>
+                .stProgress > div > div > div > div {
+                    background-color: green;
+                }
+            </style>""",
+            unsafe_allow_html=True,
+        )
+
+        my_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.001)
+            my_bar.progress(percent_complete + 1)
+        
+        my_bar.empty()
+        
         output = predict(gender, age, hypertension, heart_disease, ever_married, work_type, residence_type, avg_glucose_level, smoking)
         if output == 1:
             st.error('You have high probability for having a stroke')
